d972fdd7240e6960da7651df9c0b4832b242166b
Artyom <artemisios2014@gmail.com>
2020-07-27 22:44:42 +1000

    fix

--- js/app.js
+++ js/app.js
@@ -1,81 +1,82 @@
-﻿$(document).ready(function () {
+﻿// https://flaviocopes.com/dom-ready/
 
-    var lsel = $("#leftSelect");
-    var rsel = $("#rightSelect");
-    var grid = $("#grid");
-    var nthFormulaField =  $("#selector");
-    var nthFormula =  nthFormulaField.val();
-    panel = $("#main-panel");
+document.addEventListener('DOMContentLoaded', (event) => {
+    let lsel = $("#leftSelect");
+    let rsel = $("#rightSelect");
+    let grid = $("#grid");
+    let nthFormulaField =  $("#selector");
+    let nthFormula =  nthFormulaField.val();
+    let panel = $("#main-panel");
 	
-	
-	
-    app.config(lsel,rsel);
+
+
+    appModule.config(lsel,rsel);
     lsel.val(2);// 2 x 3
     rsel.val(3); // 2 x 3
 
 
-    app.gridChange(grid,lsel,rsel,nthFormula = 0);
+    appModule.gridChange(grid,lsel,rsel,nthFormula = 0);
 
     lsel.change(function () {
-        app.gridChange(grid,lsel,rsel,nthFormula);
+        appModule.gridChange(grid,lsel,rsel,nthFormula);
     });
     rsel.change(function () {
-        app.gridChange(grid,lsel,rsel,nthFormula);
+        appModule.gridChange(grid,lsel,rsel,nthFormula);
     });
     nthFormulaField.keyup(function () {
-
         nthFormula = nthFormulaField.val();
-        app.gridChange(grid, lsel,rsel,nthFormula);
+        appModule.gridChange(grid, lsel,rsel,nthFormula);
     });
 
 	
 	$(window).resize(function() {
 		//grid = $("#grid");
 		console.log("grid " + grid.width());
-		app.gridChange(grid,lsel,rsel,nthFormula);
+		appModule.gridChange(grid,lsel,rsel,nthFormula);
 	});
 
-	
+
+
 });
 
-
-
-
- app = {
-    config:function (firstSelectList,secondSelectList) {
-        firstSelectList.find("option").remove();
-        secondSelectList.find("option").remove();
-
-        for(i=1;i<=10;i++){
-            firstSelectList.append("<option>" + i +"</option>");
-            secondSelectList.append("<option>" + i + "</option>");
-        }
-    },
-
-    gridChange:function (grid,firstSelectList,secondSelectList,nthFormula) {
-
-        //чистим
-        grid.find("div").remove();
-
-        var row = firstSelectList.val();
-        var col = secondSelectList.val();
-        var divWidth = (grid.width() / col) - 7;
-        var counter = 0;
-
-        for (i = 0; i < row; i++) {
-            for (j = 0; j < col; j++) {
-                counter++;
-                grid.append("<div style='width:"+ divWidth + "px'>" + counter +"</div>")
+var appModule = (function () {
+    //init
+    return({
+        config: function (firstSelectList,secondSelectList) {
+            firstSelectList.find("option").remove();
+            secondSelectList.find("option").remove();
+    
+            for(i=1;i<=10;i++){
+                firstSelectList.append("<option>" + i +"</option>");
+                secondSelectList.append("<option>" + i + "</option>");
             }
+        },
+        gridChange: function (grid,firstSelectList,secondSelectList,nthFormula) {
+
+            //чистим
+            grid.find("div").remove();
+    
+            var row = firstSelectList.val();
+            var col = secondSelectList.val();
+            var divWidth = (grid.width() / col) - 7;
+            var counter = 0;
+    
+            for (i = 0; i < row; i++) {
+                for (j = 0; j < col; j++) {
+                    counter++;
+                    grid.append("<div style='width:"+ divWidth + "px'>" + counter +"</div>")
+                }
+            }
+            this.changeNthSelector(nthFormula,grid);
         }
-        app.changeNthSelector(nthFormula,grid);
-    },
 
-    changeNthSelector : function (nthFormula, grid) {
-        grid.find(':nth-child(' + nthFormula + ')').addClass("fill");
-    }
 
-};
+    });
+
+})();
+
+
+
 
 
 
